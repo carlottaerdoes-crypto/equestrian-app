443ed026c544d825251aefdebc1c38b3aa74eb97
carlottaerdoes-crypto <carlotta.erdoes@gmail.com>
2026-04-18 09:12:21 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,15 @@
-// Minimal service worker
-const CACHE = 'equestrian-fit-v1';
+const CACHE = 'equestrian-fit-v2';
+const ASSETS = ['/', '/index.html', '/style.css', '/script.js', '/manifest.json'];
 
-self.addEventListener('install', e => self.skipWaiting());
-self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+    .then(() => self.clients.claim())
+  );
+});
 self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(r => r || fetch(e.request).then(res => {
